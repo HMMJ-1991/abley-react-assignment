e05ffb7e79d1feb3c1e288847320a99aefb9b142
HMMJ-1991 <jayanihellarawa@gmail.com>
2023-10-24 02:07:06 +1300

    Display map view

--- display-map/src/components/Content.js
+++ display-map/src/components/Content.js
@@ -2,13 +2,23 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+// Default import
+import WebMap from "@arcgis/core/WebMap.js";
+
+// Namespace import
+import * as projection from "@arcgis/core/geometry/projection.js";
+
+import MapView from "@arcgis/core/views/MapView.js";
 
 export default function Content() {
   return (
+    
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="m">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '80vh' }} />
+        <Box sx={{ bgcolor: '#cfe8fc', height: '80vh' }}>
+            <div id="map"></div>
+        </Box>
       </Container>
     </React.Fragment>
   );
